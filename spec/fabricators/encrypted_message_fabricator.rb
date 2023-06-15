a01031651dd654440bb8cb7ee6e9fff80ff2e500
Matt Jankowski <matt@jankowski.online>
2023-06-15 11:54:57 -0400

    Remove faker gem

--- spec/fabricators/encrypted_message_fabricator.rb
+++ spec/fabricators/encrypted_message_fabricator.rb
@@ -3,5 +3,5 @@
 Fabricator(:encrypted_message) do
   device { Fabricate.build(:device) }
   from_account { Fabricate.build(:account) }
-  from_device_id { Faker::Number.number(digits: 5) }
+  from_device_id { '12345' }
 end
