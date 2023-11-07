baca31aaca1ec2cc2fab1838ef3ab2f8bc24dcf3
Matt Jankowski <matt@jankowski.online>
2023-11-07 17:08:33 -0500

    Better testing tlds

--- spec/fabricators/preview_card_provider_fabricator.rb
+++ spec/fabricators/preview_card_provider_fabricator.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Fabricator(:preview_card_provider) do
-  domain { sequence(:domain) { |i| "host-#{i}-name.com" } }
+  domain { sequence(:domain) { |i| "host-#{i}-name.example" } }
 end
