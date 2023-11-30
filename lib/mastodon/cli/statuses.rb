c583df9015f4517ca0a85c550f8c3a3cad98442c
Matt Jankowski <matt@jankowski.online>
2023-11-30 10:43:15 -0500

    Use private method scopes in statuses

--- lib/mastodon/cli/statuses.rb
+++ lib/mastodon/cli/statuses.rb
@@ -117,12 +117,11 @@ module Mastodon::CLI
 
       say('Beginning removal of now-orphaned media attachments to free up disk space...')
 
-      scope     = MediaAttachment.unattached.created_before(options[:days].pred.days.ago)
       processed = 0
       removed   = 0
-      progress  = create_progress_bar(scope.count)
+      progress  = create_progress_bar(aged_media_attachments.count)
 
-      scope.find_each do |media_attachment|
+      aged_media_attachments.find_each do |media_attachment|
         media_attachment.destroy!
 
         removed += 1
@@ -136,6 +135,10 @@ module Mastodon::CLI
       progress.stop
 
       say("Done after #{Time.now.to_f - start_at}s, removed #{removed} out of #{processed} media_attachments.", :green)
+    end
+
+    def aged_media_attachments
+      MediaAttachment.unattached.created_before(options[:days].pred.days.ago)
     end
 
     def remove_orphans_conversations
