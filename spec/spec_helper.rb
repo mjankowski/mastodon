d712ba66f3cbb5d96f9a45d23fdc60d64ba7b39a
Matt Jankowski <matt@jankowski.online>
2023-12-18 15:37:01 -0500

    Enable ruby performance warnings in spec runs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+Warning[:performance] = true
+
 RSpec.configure do |config|
   config.example_status_persistence_file_path = 'tmp/rspec/examples.txt'
   config.expect_with :rspec do |expectations|
