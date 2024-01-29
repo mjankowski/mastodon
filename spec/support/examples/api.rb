37ecba77190133de6d4a1f727c7d041fd105c8eb
Matt Jankowski <matt@jankowski.online>
2024-01-29 09:05:51 -0500

    Remove unneeded conditional check in shared api example

--- spec/support/examples/api.rb
+++ spec/support/examples/api.rb
@@ -4,9 +4,6 @@ RSpec.shared_examples 'forbidden for wrong scope' do |wrong_scope|
   let(:scopes) { wrong_scope }
 
   it 'returns http forbidden' do
-    # Some examples have a subject which needs to be called to make a request
-    subject if request.nil?
-
     expect(response).to have_http_status(403)
   end
 end
@@ -15,9 +12,6 @@ RSpec.shared_examples 'forbidden for wrong role' do |wrong_role|
   let(:role) { UserRole.find_by(name: wrong_role) }
 
   it 'returns http forbidden' do
-    # Some examples have a subject which needs to be called to make a request
-    subject if request.nil?
-
     expect(response).to have_http_status(403)
   end
 end
