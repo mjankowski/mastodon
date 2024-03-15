98dce5f7bd0b677012a2472c37d180424659f5ff
Matt Jankowski <matt@jankowski.online>
2024-03-15 16:04:38 -0400

    Restore composed scope approach

--- app/models/account_suggestions/friends_of_friends_source.rb
+++ app/models/account_suggestions/friends_of_friends_source.rb
@@ -7,38 +7,26 @@ class AccountSuggestions::FriendsOfFriendsSource < AccountSuggestions::Source
   end
 
   def source_query(account, limit: DEFAULT_LIMIT)
-    Account.find_by_sql([<<~SQL.squish, { id: account.id, limit: limit }]).map { |row| [row.id, row.frequency, row.followers_count] }
-      WITH first_degree AS (
-          SELECT target_account_id
-          FROM follows
-          JOIN accounts AS target_accounts ON follows.target_account_id = target_accounts.id
-          WHERE account_id = :id
-            AND NOT target_accounts.hide_collections
-      )
-      SELECT accounts.id, COUNT(*) AS frequency, account_stats.followers_count as followers_count
-      FROM accounts
-      JOIN follows ON follows.target_account_id = accounts.id
-      JOIN account_stats ON account_stats.account_id = accounts.id
-      LEFT OUTER JOIN follow_recommendation_mutes ON follow_recommendation_mutes.target_account_id = accounts.id AND follow_recommendation_mutes.account_id = :id
-      WHERE follows.account_id IN (SELECT * FROM first_degree)
-        AND NOT EXISTS (SELECT 1 FROM blocks b WHERE b.target_account_id = follows.target_account_id AND b.account_id = :id)
-        AND NOT EXISTS (SELECT 1 FROM blocks b WHERE b.target_account_id = :id AND b.account_id = follows.target_account_id)
-        AND NOT EXISTS (SELECT 1 FROM mutes m WHERE m.target_account_id = follows.target_account_id AND m.account_id = :id)
-        AND (accounts.domain IS NULL OR NOT EXISTS (SELECT 1 FROM account_domain_blocks b WHERE b.account_id = :id AND b.domain = accounts.domain))
-        AND NOT EXISTS (SELECT 1 FROM follows f WHERE f.target_account_id = follows.target_account_id AND f.account_id = :id)
-        AND follows.target_account_id <> :id
-        AND accounts.discoverable
-        AND accounts.suspended_at IS NULL
-        AND accounts.silenced_at IS NULL
-        AND accounts.moved_to_account_id IS NULL
-        AND follow_recommendation_mutes.target_account_id IS NULL
-      GROUP BY accounts.id, account_stats.id
-      ORDER BY frequency DESC, account_stats.followers_count ASC
-      LIMIT :limit
-    SQL
+    base_account_scope(account)
+      .joins(:account_stat)
+      .with(first_degree: account.following.where.not(hide_collections: true).select(:id).reorder(nil))
+      .joins(first_degree_follows_join)
+      .group('accounts.id, account_stats.id')
+      .reorder(frequency: :desc, followers_count: :asc)
+      .limit(limit)
+      .pluck(Arel.sql('accounts.id, COUNT(*) AS frequency, followers_count'))
   end
 
   private
+
+  def first_degree_follows_join
+    Arel.sql(
+      <<~SQL.squish
+        JOIN follows first_degree_follows ON first_degree_follows.target_account_id = accounts.id
+          AND first_degree_follows.account_id IN (SELECT * FROM first_degree)
+      SQL
+    )
+  end
 
   def key
     :friends_of_friends
