5e72c15fa0e486c45e5f4dde3f16246b8cd769a7
Matt Jankowski <matt@jankowski.online>
2024-03-21 14:10:05 -0400

    Update with safety_assured where needed

--- db/migrate/20240321180516_add_missing_index_to_blocked_executions.solid_queue.rb
+++ db/migrate/20240321180516_add_missing_index_to_blocked_executions.solid_queue.rb
@@ -3,6 +3,8 @@
 # This migration comes from solid_queue (originally 20240110143450)
 class AddMissingIndexToBlockedExecutions < ActiveRecord::Migration[7.1]
   def change
-    add_index :solid_queue_blocked_executions, [:concurrency_key, :priority, :job_id], name: 'index_solid_queue_blocked_executions_for_release'
+    safety_assured do
+      add_index :solid_queue_blocked_executions, [:concurrency_key, :priority, :job_id], name: 'index_solid_queue_blocked_executions_for_release'
+    end
   end
 end
