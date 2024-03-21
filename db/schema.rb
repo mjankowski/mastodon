ce9512c9bd3506ed2c0d316818868d7e47471460
Matt Jankowski <matt@jankowski.online>
2024-03-21 13:50:09 -0400

    Add and run solid_cache migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_20_163441) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_21_174662) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -1006,6 +1006,17 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_20_163441) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["version"], name: "index_software_updates_on_version", unique: true
+  end
+
+  create_table "solid_cache_entries", force: :cascade do |t|
+    t.binary "key", null: false
+    t.binary "value", null: false
+    t.datetime "created_at", null: false
+    t.bigint "key_hash", null: false
+    t.integer "byte_size", null: false
+    t.index ["byte_size"], name: "index_solid_cache_entries_on_byte_size"
+    t.index ["key_hash", "byte_size"], name: "index_solid_cache_entries_on_key_hash_and_byte_size"
+    t.index ["key_hash"], name: "index_solid_cache_entries_on_key_hash", unique: true
   end
 
   create_table "status_edits", force: :cascade do |t|
