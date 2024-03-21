eb40e835899bf9e29f4efdc0ebe0c2fb27c5c4fa
Matt Jankowski <matt@jankowski.online>
2024-03-21 15:44:31 -0400

    Replace `sidekiq_process_check` i18n key with `solid_queue_process_check`

--- app/lib/admin/system_check/sidekiq_process_check.rb
+++ app/lib/admin/system_check/sidekiq_process_check.rb
@@ -19,7 +19,7 @@ class Admin::SystemCheck::SidekiqProcessCheck < Admin::SystemCheck::BaseCheck
   end
 
   def message
-    Admin::SystemCheck::Message.new(:sidekiq_process_check, missing_queues.join(', '))
+    Admin::SystemCheck::Message.new(:solid_queue_process_check, missing_queues.join(', '))
   end
 
   private
