2de8256a2f71cb67d6af87c3e0398c7bcc01472d
Matt Jankowski <matt@jankowski.online>
2024-03-21 13:50:09 -0400

    Add and run solid_cache migrations

--- db/schema.rb
+++ db/schema.rb
@@ -1015,6 +1015,17 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_13_171909) do
     t.index ["version"], name: "index_software_updates_on_version", unique: true
   end
 
+  create_table "solid_cache_entries", force: :cascade do |t|
+    t.binary "key", null: false
+    t.binary "value", null: false
+    t.datetime "created_at", null: false
+    t.bigint "key_hash", null: false
+    t.integer "byte_size", null: false
+    t.index ["byte_size"], name: "index_solid_cache_entries_on_byte_size"
+    t.index ["key_hash", "byte_size"], name: "index_solid_cache_entries_on_key_hash_and_byte_size"
+    t.index ["key_hash"], name: "index_solid_cache_entries_on_key_hash", unique: true
+  end
+
   create_table "status_edits", force: :cascade do |t|
     t.bigint "status_id", null: false
     t.bigint "account_id"
