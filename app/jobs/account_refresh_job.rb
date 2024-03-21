c86932533ef7c2bfbe52a363458de657229f1cbe
Matt Jankowski <matt@jankowski.online>
2024-03-21 15:20:11 -0400

    Move AccountRefreshWorker to AccountRefreshJob

--- app/jobs/account_refresh_job.rb
+++ app/jobs/account_refresh_job.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
-class AccountRefreshWorker
-  include Sidekiq::Worker
-
-  sidekiq_options queue: 'pull', retry: 3, dead: false, lock: :until_executed, lock_ttl: 1.day.to_i
+class AccountRefreshJob < ApplicationJob
+  queue_as :pull
+  # sidekiq_options queue: 'pull', retry: 3, dead: false, lock: :until_executed, lock_ttl: 1.day.to_i
 
   def perform(account_id)
     account = Account.find_by(id: account_id)
