0ce69e3b209b0e96a345762b0f51e940c157b769
Matt Jankowski <matt@jankowski.online>
2024-03-21 13:52:08 -0400

    Replace redis with solid cache in config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,8 +56,8 @@ Rails.application.configure do
   # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]
 
-  # Use a different cache store in production.
-  config.cache_store = :redis_cache_store, REDIS_CACHE_PARAMS
+  # Use `solid_cache` store in production.
+  config.cache_store = :solid_cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
