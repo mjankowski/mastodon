c5dd7f9f81e6945ad8f471501c811b6cf7df2697
Matt Jankowski <matt@jankowski.online>
2024-05-02 13:45:55 -0400

    Updates hostnames to match capybara config

--- spec/lib/ostatus/tag_manager_spec.rb
+++ spec/lib/ostatus/tag_manager_spec.rb
@@ -5,13 +5,13 @@ require 'rails_helper'
 describe OStatus::TagManager do
   describe '#unique_tag' do
     it 'returns a unique tag' do
-      expect(described_class.instance.unique_tag(Time.utc(2000), 12, 'Status')).to eq 'tag:cb6e6126.ngrok.io,2000-01-01:objectId=12:objectType=Status'
+      expect(described_class.instance.unique_tag(Time.utc(2000), 12, 'Status')).to eq 'tag:localhost:3000,2000-01-01:objectId=12:objectType=Status'
     end
   end
 
   describe '#unique_tag_to_local_id' do
     it 'returns the ID part' do
-      expect(described_class.instance.unique_tag_to_local_id('tag:cb6e6126.ngrok.io,2000-01-01:objectId=12:objectType=Status', 'Status')).to eql '12'
+      expect(described_class.instance.unique_tag_to_local_id('tag:localhost:3000,2000-01-01:objectId=12:objectType=Status', 'Status')).to eql '12'
     end
 
     it 'returns nil if it is not local id' do
@@ -19,17 +19,17 @@ describe OStatus::TagManager do
     end
 
     it 'returns nil if it is not expected type' do
-      expect(described_class.instance.unique_tag_to_local_id('tag:cb6e6126.ngrok.io,2000-01-01:objectId=12:objectType=Block', 'Status')).to be_nil
+      expect(described_class.instance.unique_tag_to_local_id('tag:localhost:3000,2000-01-01:objectId=12:objectType=Block', 'Status')).to be_nil
     end
 
     it 'returns nil if it does not have object ID' do
-      expect(described_class.instance.unique_tag_to_local_id('tag:cb6e6126.ngrok.io,2000-01-01:objectType=Status', 'Status')).to be_nil
+      expect(described_class.instance.unique_tag_to_local_id('tag:localhost:3000,2000-01-01:objectType=Status', 'Status')).to be_nil
     end
   end
 
   describe '#local_id?' do
     it 'returns true for a local ID' do
-      expect(described_class.instance.local_id?('tag:cb6e6126.ngrok.io;objectId=12:objectType=Status')).to be true
+      expect(described_class.instance.local_id?('tag:localhost:3000;objectId=12:objectType=Status')).to be true
     end
 
     it 'returns false for a foreign ID' do
@@ -63,7 +63,7 @@ describe OStatus::TagManager do
 
       it 'returns the URL for account' do
         expect(target.object_type).to eq :person
-        expect(subject).to eq 'https://cb6e6126.ngrok.io/users/alice'
+        expect(subject).to eq 'http://localhost:3000/users/alice'
       end
     end
   end
