7719c551f446bb5902d3fdf548655f62d9f3a148
Matt Jankowski <matt@jankowski.online>
2024-05-20 09:32:49 -0400

    Remove limit from filter params in admin/accounts controller

--- app/controllers/api/v2/admin/accounts_controller.rb
+++ app/controllers/api/v2/admin/accounts_controller.rb
@@ -2,16 +2,16 @@
 
 class Api::V2::Admin::AccountsController < Api::V1::Admin::AccountsController
   FILTER_PARAMS = %i(
-    origin
-    status
-    permissions
-    username
     by_domain
     display_name
     email
-    ip
     invited_by
-    role_ids
+    ip
+    origin
+    permissions
+    status
+    username
+    role_ids: []
   ).freeze
 
   PAGINATION_PARAMS = (%i(limit) + FILTER_PARAMS).freeze
@@ -39,10 +39,15 @@ class Api::V2::Admin::AccountsController < Api::V1::Admin::AccountsController
   end
 
   def filter_params
-    params.permit(*FILTER_PARAMS, role_ids: [])
+    params
+      .slice(:by_domain, :display_name, :email, :invited_by, :ip, :origin, :permissions, :status, :username, :role_ids)
+      .permit(*FILTER_PARAMS)
   end
 
   def pagination_params(core_params)
-    params.slice(*PAGINATION_PARAMS).permit(*PAGINATION_PARAMS).merge(core_params)
+    params
+      .slice(*PAGINATION_PARAMS)
+      .permit(*PAGINATION_PARAMS)
+      .merge(core_params)
   end
 end
