11ccd1645e191c79410238b7d071d83e138f05b9
Matt Jankowski <matt@jankowski.online>
2024-05-20 13:36:10 -0400

    Extract permitted params constant in v1/apps

--- app/controllers/api/v1/apps_controller.rb
+++ app/controllers/api/v1/apps_controller.rb
@@ -3,6 +3,15 @@
 class Api::V1::AppsController < Api::BaseController
   skip_before_action :require_authenticated_user!
 
+  PERMITTED_PARAMS = [
+    :client_name,
+    :redirect_uris,
+    :scopes,
+    :website,
+    redirect_uris: [],
+    scopes: [],
+  ].freeze
+
   def create
     @app = Doorkeeper::Application.create!(application_options)
     render json: @app, serializer: REST::CredentialApplicationSerializer
@@ -20,10 +29,12 @@ class Api::V1::AppsController < Api::BaseController
   end
 
   def app_scopes_or_default
-    app_params[:scopes] || Doorkeeper.configuration.default_scopes
+    Array(app_params[:scopes]).first || Doorkeeper.configuration.default_scopes
   end
 
   def app_params
-    params.permit(:client_name, :scopes, :website, :redirect_uris, redirect_uris: [])
+    params
+      .slice(*PERMITTED_PARAMS)
+      .permit(*PERMITTED_PARAMS)
   end
 end
