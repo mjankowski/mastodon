495711915f36ef1b90a2b45b5af892ccb761cfed
Matt Jankowski <matt@jankowski.online>
2024-07-24 11:28:54 -0400

    Remove exclusion for `Rails/ReversibleMigration` cop

--- db/migrate/20220827195229_change_canonical_email_blocks_nullable.rb
+++ db/migrate/20220827195229_change_canonical_email_blocks_nullable.rb
@@ -2,6 +2,6 @@
 
 class ChangeCanonicalEmailBlocksNullable < ActiveRecord::Migration[6.1]
   def change
-    safety_assured { change_column :canonical_email_blocks, :reference_account_id, :bigint, null: true, default: nil }
+    safety_assured { change_column_null :canonical_email_blocks, :reference_account_id, true }
   end
 end
