38141036bc010ca9901e44a22a0deef8d4fd1992
Matt Jankowski <matt@jankowski.online>
2024-09-14 13:45:13 -0400

    Use CTE `with` in `AnnualReport::Percentiles` queries

--- app/lib/annual_report/percentiles.rb
+++ app/lib/annual_report/percentiles.rb
@@ -21,35 +21,49 @@ class AnnualReport::Percentiles < AnnualReport::Source
   end
 
   def total_with_fewer_followers
-    @total_with_fewer_followers ||= Follow.find_by_sql([<<~SQL.squish, { year: @year, comparison: followers_gained }]).first.total
-      WITH tmp0 AS (
-        SELECT follows.target_account_id
-        FROM follows
-        INNER JOIN accounts ON accounts.id = follows.target_account_id
-        WHERE date_part('year', follows.created_at) = :year
-          AND accounts.domain IS NULL
-        GROUP BY follows.target_account_id
-        HAVING COUNT(*) < :comparison
-      )
-      SELECT count(*) AS total
-      FROM tmp0
-    SQL
+    @total_with_fewer_followers ||= fewer_followers.to_a.first.total
   end
 
   def total_with_fewer_statuses
-    @total_with_fewer_statuses ||= Status.find_by_sql([<<~SQL.squish, { comparison: statuses_created, min_id: year_as_snowflake_range.first, max_id: year_as_snowflake_range.last }]).first.total
-      WITH tmp0 AS (
-        SELECT statuses.account_id
-        FROM statuses
-        INNER JOIN accounts ON accounts.id = statuses.account_id
-        WHERE statuses.id BETWEEN :min_id AND :max_id
-          AND accounts.domain IS NULL
-        GROUP BY statuses.account_id
-        HAVING count(*) < :comparison
-      )
-      SELECT count(*) AS total
-      FROM tmp0
-    SQL
+    @total_with_fewer_statuses ||= fewer_statuses.to_a.first.total
+  end
+
+  def fewer_followers
+    Follow
+      .with(under_threshold_follows:)
+      .from('under_threshold_follows')
+      .select(Arel.star.count.as('total'))
+  end
+
+  def fewer_statuses
+    Status
+      .unscoped # Remove the default order and `kept` conditions
+      .with(under_threshold_statuses:)
+      .from('under_threshold_statuses')
+      .select(Arel.star.count.as('total'))
+  end
+
+  def under_threshold_follows
+    Follow
+      .joins(:account)
+      .merge(Account.local)
+      .where(<<~SQL.squish, year: @year)
+        DATE_PART('year', follows.created_at)::int = :year
+      SQL
+      .group(:target_account_id)
+      .having(Arel.star.count.lt(followers_gained))
+      .select(:target_account_id)
+  end
+
+  def under_threshold_statuses
+    Status
+      .unscoped # Remove the default order and `kept` conditions
+      .joins(:account)
+      .merge(Account.local)
+      .where(statuses: { id: year_as_snowflake_range.first..year_as_snowflake_range.last })
+      .group(:account_id)
+      .having(Arel.star.count.lt(statuses_created))
+      .select(:account_id)
   end
 
   def total_with_any_followers
