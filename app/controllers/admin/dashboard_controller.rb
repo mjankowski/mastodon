e8c75c50d448b78403a00ad04e363db6209654ed
Matt Jankowski <matt@jankowski.online>
2024-10-17 09:31:26 -0400

    Add constant for time period start to admin dashboard controller

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,8 @@
 
 module Admin
   class DashboardController < BaseController
+    TIME_PERIOD_RANGE = 29.days
+
     include Redisable
 
     def index
@@ -9,10 +11,10 @@ module Admin
 
       @pending_appeals_count = Appeal.pending.async_count
       @pending_reports_count = Report.unresolved.async_count
-      @pending_tags_count    = Tag.pending_review.async_count
-      @pending_users_count   = User.pending.async_count
-      @system_checks         = Admin::SystemCheck.perform(current_user)
-      @time_period           = (29.days.ago.to_date...Time.now.utc.to_date)
+      @pending_tags_count = Tag.pending_review.async_count
+      @pending_users_count = User.pending.async_count
+      @system_checks = Admin::SystemCheck.perform(current_user)
+      @time_period = (TIME_PERIOD_RANGE.ago.to_date...Time.now.utc.to_date)
     end
   end
 end
