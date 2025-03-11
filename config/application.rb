4d4acdebce9713d98c3cf7246561d0d2cc365312
Matt Jankowski <matt@jankowski.online>
2025-03-11 13:48:04 -0400

    Enable autoload lib

--- config/application.rb
+++ config/application.rb
@@ -64,7 +64,7 @@ module Mastodon
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    # config.autoload_lib(ignore: %w(assets tasks templates generators linter paperclip))
+    config.autoload_lib(ignore: %w(assets tasks templates generators linter paperclip))
     # TODO: We should enable this eventually, but for now there are many things
     # in the wrong path from the perspective of zeitwerk.
 
