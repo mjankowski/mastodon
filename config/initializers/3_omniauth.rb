d75e9b2bc56dc6075e40cfeab2b23a1fc25a18b4
Matt Jankowski <matt@jankowski.online>
2025-06-12 12:22:51 -0400

    Move omniauth CAS options to yaml config

--- config/initializers/3_omniauth.rb
+++ config/initializers/3_omniauth.rb
@@ -11,31 +11,8 @@ end
 Devise.setup do |config|
   # CAS strategy
   if Rails.configuration.x.omniauth.cas_enabled?
-    cas_options = {}
-    cas_options[:display_name] = ENV.fetch('CAS_DISPLAY_NAME', nil)
-    cas_options[:url] = ENV['CAS_URL'] if ENV['CAS_URL']
-    cas_options[:host] = ENV['CAS_HOST'] if ENV['CAS_HOST']
-    cas_options[:port] = ENV['CAS_PORT'] if ENV['CAS_PORT']
-    cas_options[:ssl] = ENV['CAS_SSL'] == 'true' if ENV['CAS_SSL']
-    cas_options[:service_validate_url] = ENV['CAS_VALIDATE_URL'] if ENV['CAS_VALIDATE_URL']
-    cas_options[:callback_url] = ENV['CAS_CALLBACK_URL'] if ENV['CAS_CALLBACK_URL']
-    cas_options[:logout_url] = ENV['CAS_LOGOUT_URL'] if ENV['CAS_LOGOUT_URL']
-    cas_options[:login_url] = ENV['CAS_LOGIN_URL'] if ENV['CAS_LOGIN_URL']
-    cas_options[:uid_field] = ENV['CAS_UID_FIELD'] || 'user' if ENV['CAS_UID_FIELD']
-    cas_options[:ca_path] = ENV['CAS_CA_PATH'] if ENV['CAS_CA_PATH']
-    cas_options[:disable_ssl_verification] = ENV['CAS_DISABLE_SSL_VERIFICATION'] == 'true'
-    cas_options[:uid_key] = ENV['CAS_UID_KEY'] || 'user'
-    cas_options[:name_key] = ENV['CAS_NAME_KEY'] || 'name'
-    cas_options[:email_key] = ENV['CAS_EMAIL_KEY'] || 'email'
-    cas_options[:nickname_key] = ENV['CAS_NICKNAME_KEY'] || 'nickname'
-    cas_options[:first_name_key] = ENV['CAS_FIRST_NAME_KEY'] || 'firstname'
-    cas_options[:last_name_key] = ENV['CAS_LAST_NAME_KEY'] || 'lastname'
-    cas_options[:location_key] = ENV['CAS_LOCATION_KEY'] || 'location'
-    cas_options[:image_key] = ENV['CAS_IMAGE_KEY'] || 'image'
-    cas_options[:phone_key] = ENV['CAS_PHONE_KEY'] || 'phone'
-    cas_options[:security] = {}
-    cas_options[:security][:assume_email_is_verified] = ENV['CAS_SECURITY_ASSUME_EMAIL_IS_VERIFIED'] == 'true'
-    config.omniauth :cas, cas_options
+    config.omniauth :cas,
+                    Rails.configuration.x.omniauth.cas_options.compact
   end
 
   # SAML strategy
