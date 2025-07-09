4d320a8d4896b77db840306d9e60a4d7323f15d7
Matt Jankowski <matt@jankowski.online>
2025-07-09 13:29:10 -0400

    The lines where it ends with "end," really irritate me

--- app/lib/annual_report/most_used_apps.rb
+++ app/lib/annual_report/most_used_apps.rb
@@ -5,16 +5,20 @@ class AnnualReport::MostUsedApps < AnnualReport::Source
 
   def generate
     {
-      most_used_apps: most_used_apps.map do |(name, count)|
-                        {
-                          name: name,
-                          count: count,
-                        }
-                      end,
+      most_used_apps: used_apps_map,
     }
   end
 
   private
+
+  def used_apps_map
+    most_used_apps.map do |name, count|
+      {
+        name: name,
+        count: count,
+      }
+    end
+  end
 
   def most_used_apps
     report_statuses.joins(:application).group(oauth_applications: [:name]).order(count_all: :desc).limit(SET_SIZE).count
