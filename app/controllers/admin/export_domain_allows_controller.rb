518295ff1ad18a1c25100798d08a730f878c76ea
Matt Jankowski <matt@jankowski.online>
2025-07-22 15:59:02 -0400

    WIP on metrics and misc cleanup

--- app/controllers/admin/export_domain_allows_controller.rb
+++ app/controllers/admin/export_domain_allows_controller.rb
@@ -6,10 +6,18 @@ module Admin
   class ExportDomainAllowsController < BaseController
     include Admin::ExportControllerConcern
 
-    before_action :set_dummy_import!, only: [:new]
+    before_action :authorize_domain_allow_create, only: [:new, :import]
+    with_options only: :import do
+      before_action :set_import
+      before_action :validate_import
+    end
+
+    rescue_from ActionController::ParameterMissing do
+      redirect_to admin_instances_path, flash: { error: t('admin.export_domain_allows.no_file') }
+    end
 
     def new
-      authorize :domain_allow, :create?
+      @import = Admin::Import.new
     end
 
     def export
@@ -18,26 +26,30 @@ module Admin
     end
 
     def import
-      authorize :domain_allow, :create?
-      begin
-        @import = Admin::Import.new(import_params)
-        return render :new unless @import.validate
+      @import.csv_rows.each do |row|
+        domain = row['#domain'].strip
+        next if DomainAllow.allowed?(domain)
 
-        @import.csv_rows.each do |row|
-          domain = row['#domain'].strip
-          next if DomainAllow.allowed?(domain)
-
-          domain_allow = DomainAllow.new(domain: domain)
-          log_action :create, domain_allow if domain_allow.save
-        end
-        flash[:notice] = I18n.t('admin.domain_allows.created_msg')
-      rescue ActionController::ParameterMissing
-        flash[:error] = I18n.t('admin.export_domain_allows.no_file')
+        domain_allow = DomainAllow.new(domain: domain)
+        log_action :create, domain_allow if domain_allow.save
       end
-      redirect_to admin_instances_path
+
+      redirect_to admin_instances_path, notice: t('admin.domain_allows.created_msg')
     end
 
     private
+
+    def authorize_domain_allow_create
+      authorize :domain_allow, :create?
+    end
+
+    def set_import
+      @import = Admin::Import.new(import_params)
+    end
+
+    def validate_import
+      render :new unless @import.validate
+    end
 
     def export_filename
       'domain_allows.csv'
