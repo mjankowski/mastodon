5e8305cbe458c65876d9add08ea715661602b3a6
Matt Jankowski <matt@jankowski.online>
2025-08-01 17:12:31 -0400

    Add `StatusDescriptionPresenter` to summarize status detail

--- app/helpers/statuses_helper.rb
+++ app/helpers/statuses_helper.rb
@@ -14,38 +14,6 @@ module StatusesHelper
     end
   end
 
-  def media_summary(status)
-    attachments = { image: 0, video: 0, audio: 0 }
-
-    status.ordered_media_attachments.each do |media|
-      if media.video?
-        attachments[:video] += 1
-      elsif media.audio?
-        attachments[:audio] += 1
-      else
-        attachments[:image] += 1
-      end
-    end
-
-    text = attachments.to_a.reject { |_, value| value.zero? }.map { |key, value| I18n.t("statuses.attached.#{key}", count: value) }.join(' · ')
-
-    return if text.blank?
-
-    I18n.t('statuses.attached.description', attached: text)
-  end
-
-  def status_text_summary(status)
-    return if status.spoiler_text.blank?
-
-    I18n.t('statuses.content_warning', warning: status.spoiler_text)
-  end
-
-  def poll_summary(status)
-    return unless status.preloadable_poll
-
-    status.preloadable_poll.options.map { |o| "[ ] #{o}" }.join("\n")
-  end
-
   def status_classnames(status, is_quote)
     if is_quote
       'status--is-quote'
@@ -55,14 +23,7 @@ module StatusesHelper
   end
 
   def status_description(status)
-    components = [[media_summary(status), status_text_summary(status)].compact_blank.join(' · ')]
-
-    if status.spoiler_text.blank?
-      components << status.text
-      components << poll_summary(status)
-    end
-
-    components.compact_blank.join("\n\n")
+    StatusDescriptionPresenter.new(status).description
   end
 
   # This logic should be kept in sync with https://github.com/mastodon/mastodon/blob/425311e1d95c8a64ddac6c724fca247b8b893a82/app/javascript/mastodon/features/status/components/card.jsx#L160
