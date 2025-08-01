32c739094e1fa1b9a827abe6aed43b2cc6d283d3
Matt Jankowski <matt@jankowski.online>
2025-08-01 17:12:31 -0400

    Add `StatusDescriptionPresenter` to summarize status detail

--- spec/helpers/statuses_helper_spec.rb
+++ spec/helpers/statuses_helper_spec.rb
@@ -3,36 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe StatusesHelper do
-  describe 'status_text_summary' do
-    context 'with blank text' do
-      let(:status) { Status.new(spoiler_text: '') }
-
-      it 'returns immediately with nil' do
-        result = helper.status_text_summary(status)
-        expect(result).to be_nil
-      end
-    end
-
-    context 'with present text' do
-      let(:status) { Status.new(spoiler_text: 'SPOILERS!!!') }
-
-      it 'returns the content warning' do
-        result = helper.status_text_summary(status)
-        expect(result).to eq(I18n.t('statuses.content_warning', warning: 'SPOILERS!!!'))
-      end
-    end
-  end
-
-  describe '#media_summary' do
-    subject { helper.media_summary(status) }
-
-    let(:status) { Fabricate.build :status }
-
-    before { %i(video audio image).each { |type| Fabricate.build :media_attachment, status:, type: } }
-
-    it { is_expected.to eq('Attached: 1 image · 1 video · 1 audio') }
-  end
-
   describe 'visibility_icon' do
     context 'with a status that is public' do
       let(:status) { Status.new(visibility: 'public') }
