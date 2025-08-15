d47e481cf75f151f60538c22332fffbfd76e33ff
Matt Jankowski <matt@jankowski.online>
2025-08-14 20:01:00 -0400

    Prepare invite codes for the private cloud

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -19,6 +19,7 @@
 class Invite < ApplicationRecord
   include Expireable
 
+  CODE_LENGTH = 8
   COMMENT_SIZE_LIMIT = 420
   ELIGIBLE_CODE_CHARACTERS = [*('a'..'z'), *('A'..'Z'), *('0'..'9')].freeze
   HOMOGLYPHS = %w(0 1 I l O).freeze
@@ -33,16 +34,26 @@ class Invite < ApplicationRecord
 
   before_validation :set_code, on: :create
 
+  delegate :functional?, allow_nil: true, prefix: true, to: :user
+
   def valid_for_use?
-    (max_uses.nil? || uses < max_uses) && !expired? && user&.functional?
+    usable? && !!user_functional?
   end
 
   private
 
+  def usable?
+    under_usage_limit? && !expired?
+  end
+
+  def under_usage_limit?
+    max_uses.blank? || uses < max_uses
+  end
+
   def set_code
     loop do
-      self.code = VALID_CODE_CHARACTERS.sample(8).join
-      break if Invite.find_by(code: code).nil?
+      self.code = VALID_CODE_CHARACTERS.sample(CODE_LENGTH).join
+      break unless Invite.exists?(code:)
     end
   end
 end
