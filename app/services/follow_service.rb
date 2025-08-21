4329160f3a2ca6b865b86ee40499d9c001644896
Matt Jankowski <matt@jankowski.online>
2025-08-21 12:52:23 -0400

    WIP

--- app/services/follow_service.rb
+++ app/services/follow_service.rb
@@ -53,7 +53,7 @@ class FollowService < BaseService
   end
 
   def following_not_allowed?
-    DomainControl.domain_not_allowed?(@target_account.domain) || @target_account.blocking?(@source_account) || @source_account.blocking?(@target_account) || @target_account.moved? || (!@target_account.local? && @target_account.ostatus?) || @source_account.domain_blocking?(@target_account.domain)
+    domain_not_allowed?(@target_account.domain) || @target_account.blocking?(@source_account) || @source_account.blocking?(@target_account) || @target_account.moved? || (!@target_account.local? && @target_account.ostatus?) || @source_account.domain_blocking?(@target_account.domain)
   end
 
   def change_follow_options!
