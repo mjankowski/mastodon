93da350f2e1cd4e6fb9620c7a32052bb3c416e6f
Matt Jankowski <matt@jankowski.online>
2025-08-27 17:09:18 -0400

    Add helpers for player media components

--- app/helpers/media_player_helper.rb
+++ app/helpers/media_player_helper.rb
@@ -3,4 +3,60 @@
 module MediaPlayerHelper
   PLAYER_HEIGHT = 380
   PLAYER_WIDTH = 670
+
+  def player_component_video(media, &block)
+    meta = media.file.meta || {}
+
+    react_component(
+      :video,
+      alt: media.description,
+      blurhash: media.blurhash,
+      detailed: true,
+      editable: true,
+      frameRate: meta.dig('original', 'frame_rate'),
+      height: PLAYER_HEIGHT,
+      inline: true,
+      media: serialized_media(media),
+      preview: media.thumbnail.present? ? media.thumbnail.url : media.file.url(:small),
+      src: media.file.url(:original),
+      width: PLAYER_WIDTH,
+      &block
+    )
+  end
+
+  def player_component_gifv(media, &block)
+    react_component(
+      :media_gallery,
+      autoplay: true,
+      height: PLAYER_HEIGHT,
+      media: serialized_media(media),
+      standalone: true,
+      &block
+    )
+  end
+
+  def player_component_audio(media, &block)
+    meta = media.file.meta || {}
+
+    react_component(
+      :audio,
+      accentColor: meta.dig('colors', 'accent'),
+      alt: media.description,
+      backgroundColor: meta.dig('colors', 'background'),
+      duration: meta.dig(:original, :duration),
+      foregroundColor: meta.dig('colors', 'foreground'),
+      fullscreen: true,
+      height: PLAYER_HEIGHT,
+      poster: media.thumbnail.present? ? media.thumbnail.url : media.account.avatar_static_url,
+      src: media.file.url(:original),
+      width: PLAYER_WIDTH,
+      &block
+    )
+  end
+
+  private
+
+  def serialized_media(media)
+    [ActiveModelSerializers::SerializableResource.new(media, serializer: REST::MediaAttachmentSerializer)].as_json
+  end
 end
