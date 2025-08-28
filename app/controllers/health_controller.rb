ab4c282da46d4293cc39a9cf35ad1ff01c314e0e
Matt Jankowski <matt@jankowski.online>
2025-08-28 13:27:24 -0400

    Inherit from rails health controller while preserving defaults

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
-class HealthController < ActionController::Base # rubocop:disable Rails/ApplicationController
-  def show
+class HealthController < Rails::HealthController
+  before_action :handle_default, if: -> { request.format.text? }
+  content_security_policy false, if: -> { request.format.html? }
+
+  private
+
+  def handle_default
     render plain: 'OK'
   end
 end
--- spec/requests/health_spec.rb
+++ spec/requests/health_spec.rb
@@ -4,13 +4,28 @@ require 'rails_helper'
 
 RSpec.describe 'Health check endpoint' do
   describe 'GET /health' do
-    it 'returns http success when server is functioning' do
-      get '/health'
+    context 'without format specified' do
+      it 'returns plain text response' do
+        get health_path
 
-      expect(response)
-        .to have_http_status(200)
-      expect(response.body)
-        .to include('OK')
+        expect(response)
+          .to have_http_status(200)
+        expect(response.body)
+          .to include('OK')
+        expect(response.media_type)
+          .to eq('text/plain')
+      end
+    end
+
+    context 'with HTML format' do
+      it 'returns HTML page' do
+        get health_path(format: :html)
+
+        expect(response)
+          .to have_http_status(200)
+        expect(response.media_type)
+          .to eq('text/html')
+      end
     end
   end
 end
