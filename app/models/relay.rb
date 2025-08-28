b0f8ced94967c8abae21e583530baa4317f5ef54
Matt Jankowski <matt@jankowski.online>
2025-08-28 09:52:45 -0400

    Lose payload var

--- app/models/relay.rb
+++ app/models/relay.rb
@@ -30,19 +30,15 @@ class Relay < ApplicationRecord
   end
 
   def enable!
-    payload = Oj.dump(follow_activity)
-
     update!(state: :pending, follow_activity_id: activity_id)
     reset_delivery_tracker
-    track_state_change(payload)
+    track_state_change Oj.dump(follow_activity)
   end
 
   def disable!
-    payload = Oj.dump(unfollow_activity)
-
     update!(state: :idle, follow_activity_id: nil)
     reset_delivery_tracker
-    track_state_change(payload)
+    track_state_change Oj.dump(unfollow_activity)
   end
 
   private
