8b0db91a4eb65781e4fd6488ed7dcb66ede9e69e
Matt Jankowski <matt@jankowski.online>
2025-09-06 13:54:40 -0400

    Method to check automated registration

--- app/validators/registration_form_time_validator.rb
+++ app/validators/registration_form_time_validator.rb
@@ -4,6 +4,12 @@ class RegistrationFormTimeValidator < ActiveModel::Validator
   REGISTRATION_FORM_MIN_TIME = 3.seconds.freeze
 
   def validate(user)
-    user.errors.add(:base, I18n.t('auth.too_fast')) if user.registration_form_time.present? && user.registration_form_time > REGISTRATION_FORM_MIN_TIME.ago
+    user.errors.add(:base, I18n.t('auth.too_fast')) if likely_automated?(user)
+  end
+
+  private
+
+  def likely_automated?(user)
+    user.registration_form_time.present? && user.registration_form_time > REGISTRATION_FORM_MIN_TIME.ago
   end
 end
