525040c37fba350fa70594de680bb5f7eb379b13
Matt Jankowski <matt@jankowski.online>
2025-09-16 11:26:07 -0400

    Coverage for specific values

--- spec/lib/registration_spec.rb
+++ spec/lib/registration_spec.rb
@@ -7,5 +7,23 @@ RSpec.describe Registration do
     subject { described_class.mode }
 
     it { is_expected.to be_a(ActiveSupport::StringInquirer) }
+
+    context 'when mode is open' do
+      before { Setting.registrations_mode = 'open' }
+
+      it { is_expected.to be_open }
+    end
+
+    context 'when mode is approved' do
+      before { Setting.registrations_mode = 'approved' }
+
+      it { is_expected.to be_approved }
+    end
+
+    context 'when mode is none' do
+      before { Setting.registrations_mode = 'none' }
+
+      it { is_expected.to be_none }
+    end
   end
 end
