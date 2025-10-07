aa078d16314274e4037670b526e5186d9acb8145
Matt Jankowski <matt@jankowski.online>
2025-10-07 11:03:52 -0400

    Prefer arel style for coalesce build in announcemwent

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -21,11 +21,7 @@ class Announcement < ApplicationRecord
 
   class << self
     def coalesced_chronology_timestamps
-      Arel.sql(
-        <<~SQL.squish
-          COALESCE(announcements.starts_at, announcements.scheduled_at, announcements.published_at, announcements.created_at)
-        SQL
-      )
+      arel_table.coalesce(arel_table[:starts_at], arel_table[:scheduled_at], arel_table[:published_at], arel_table[:created_at])
     end
   end
 
@@ -65,8 +61,8 @@ class Announcement < ApplicationRecord
 
   def reactions(account = nil)
     grouped_ordered_announcement_reactions.select(
-      [:name, :custom_emoji_id, 'COUNT(*) as count'].tap do |values|
-        values << value_for_reaction_me_column(account)
+      [:name, :custom_emoji_id, Arel.star.count.as('count')].tap do |values|
+        values << value_for_reaction_me_column(account).as('me')
       end
     ).to_a.tap do |records|
       ActiveRecord::Associations::Preloader.new(records: records, associations: :custom_emoji).call
@@ -87,16 +83,16 @@ class Announcement < ApplicationRecord
 
   def value_for_reaction_me_column(account)
     if account.nil?
-      'FALSE AS me'
+      self.class.arel_table.create_false
     else
-      <<~SQL.squish
+      Arel.sql(<<~SQL.squish)
         EXISTS(
           SELECT 1
           FROM announcement_reactions inner_reactions
           WHERE inner_reactions.account_id = #{account.id}
             AND inner_reactions.announcement_id = announcement_reactions.announcement_id
             AND inner_reactions.name = announcement_reactions.name
-        ) AS me
+        )
       SQL
     end
   end
