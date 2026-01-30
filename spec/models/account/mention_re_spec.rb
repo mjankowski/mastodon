7218e72aebbca75301388fa73984b905dc2e0f9e
Matt Jankowski <matt@jankowski.online>
2026-01-30 12:23:35 -0500

    Use nested context in account mentions

--- spec/models/account/mention_re_spec.rb
+++ spec/models/account/mention_re_spec.rb
@@ -3,43 +3,71 @@
 require 'rails_helper'
 
 RSpec.describe Account::MENTION_RE do
-  it 'matches usernames in the middle of a sentence' do
-    expect(subject.match('Hello to @alice from me')[1]).to eq 'alice'
+  describe 'Valid matches' do
+    subject { string.match(described_class)[1] }
+
+    context 'with username in the middle of a sentence' do
+      let(:string) { 'Hello to @alice from me' }
+
+      it { is_expected.to eq('alice') }
+    end
+
+    context 'with username at beginning of status' do
+      let(:string) { '@alice Hey how are you?' }
+
+      it { is_expected.to eq('alice') }
+    end
+
+    context 'with full username' do
+      let(:string) { '@alice@example.com' }
+
+      it { is_expected.to eq('alice@example.com') }
+    end
+
+    context 'with full username with dot at the end' do
+      let(:string) { 'Hello @alice@example.com.' }
+
+      it { is_expected.to eq('alice@example.com') }
+    end
+
+    context 'with dot-prepended username' do
+      let(:string) { '.@alice I want everybody to see this' }
+
+      it { is_expected.to eq('alice') }
+    end
+
+    context 'with username after the letter ß' do
+      let(:string) { 'Hello toß @alice from me' }
+
+      it { is_expected.to eq('alice') }
+    end
+
+    context 'with username including uppercase characters' do
+      let(:string) { 'Hello to @aLice@Example.com from me' }
+
+      it { is_expected.to eq('aLice@Example.com') }
+    end
   end
 
-  it 'matches usernames in the beginning of status' do
-    expect(subject.match('@alice Hey how are you?')[1]).to eq 'alice'
-  end
+  describe 'Invalid matches' do
+    subject { string.match(described_class) }
 
-  it 'matches full usernames' do
-    expect(subject.match('@alice@example.com')[1]).to eq 'alice@example.com'
-  end
+    context 'with an email address' do
+      let(:string) { 'Drop me an e-mail at alice@example.com' }
 
-  it 'matches full usernames with a dot at the end' do
-    expect(subject.match('Hello @alice@example.com.')[1]).to eq 'alice@example.com'
-  end
+      it { is_expected.to be_nil }
+    end
 
-  it 'matches dot-prepended usernames' do
-    expect(subject.match('.@alice I want everybody to see this')[1]).to eq 'alice'
-  end
+    context 'with a URL' do
+      let(:string) { 'Check this out https://medium.com/@alice/some-article#.abcdef123' }
 
-  it 'does not match e-mails' do
-    expect(subject.match('Drop me an e-mail at alice@example.com')).to be_nil
-  end
+      it { is_expected.to be_nil }
+    end
 
-  it 'does not match URLs' do
-    expect(subject.match('Check this out https://medium.com/@alice/some-article#.abcdef123')).to be_nil
-  end
+    context 'with a URL with query string' do
+      let(:string) { 'https://example.com/?x=@alice' }
 
-  it 'does not match URL query string' do
-    expect(subject.match('https://example.com/?x=@alice')).to be_nil
-  end
-
-  it 'matches usernames immediately following the letter ß' do
-    expect(subject.match('Hello toß @alice from me')[1]).to eq 'alice'
-  end
-
-  it 'matches usernames containing uppercase characters' do
-    expect(subject.match('Hello to @aLice@Example.com from me')[1]).to eq 'aLice@Example.com'
+      it { is_expected.to be_nil }
+    end
   end
 end
