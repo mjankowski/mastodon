f1f790b2afb864587b4eeed8941ec2905935f24b
Matt Jankowski <matt@jankowski.online>
2026-03-02 16:23:16 -0500

    WIP

--- spec/support/chewy.rb
+++ spec/support/chewy.rb
@@ -2,7 +2,11 @@
 
 RSpec.configure do |config|
   config.around :each, :search do |example|
+    Chewy.settings[:enabled] = true
+
     # Configure chewy to use `urgent` strategy to index documents
     Chewy.strategy(:urgent) { example.run }
+
+    Chewy.settings[:enabled] = false
   end
 end
