fed3151a6e01ab64295d94ca22706755b5a0028c
Matt Jankowski <matt@jankowski.online>
2026-03-04 13:49:03 -0500

    Move user role validation to concern

--- app/models/concerns/user/role.rb
+++ app/models/concerns/user/role.rb
@@ -2,4 +2,14 @@
 
 module User::Role
   extend ActiveSupport::Concern
+
+  included do
+    validate :validate_role_elevation, if: -> { defined?(@current_account) }
+  end
+
+  private
+
+  def validate_role_elevation
+    errors.add(:role_id, :elevated) if role&.overrides?(@current_account&.user_role)
+  end
 end
