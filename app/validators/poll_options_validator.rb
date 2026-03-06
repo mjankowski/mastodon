c70ffb9c9fd9286567b086e269ddcf7d9af5875f
Matt Jankowski <matt@jankowski.online>
2026-03-06 08:32:56 -0500

    Extract methods in poll options validator

--- app/validators/poll_options_validator.rb
+++ app/validators/poll_options_validator.rb
@@ -1,13 +1,36 @@
 # frozen_string_literal: true
 
 class PollOptionsValidator < ActiveModel::Validator
-  MAX_OPTIONS      = 4
   MAX_OPTION_CHARS = 50
+  MAX_OPTIONS = 4
+  MIN_OPTIONS = 1
 
   def validate(poll)
-    poll.errors.add(:options, I18n.t('polls.errors.too_few_options')) unless poll.options.size > 1
-    poll.errors.add(:options, I18n.t('polls.errors.too_many_options', max: MAX_OPTIONS)) if poll.options.size > MAX_OPTIONS
-    poll.errors.add(:options, I18n.t('polls.errors.over_character_limit', max: MAX_OPTION_CHARS)) if poll.options.any? { |option| option.each_grapheme_cluster.size > MAX_OPTION_CHARS }
-    poll.errors.add(:options, I18n.t('polls.errors.duplicate_options')) unless poll.options.uniq.size == poll.options.size
+    poll.errors.add(:options, too_few_message) unless poll.options.size > MIN_OPTIONS
+    poll.errors.add(:options, too_many_message) if poll.options.size > MAX_OPTIONS
+    poll.errors.add(:options, character_limit_message) if options_over_limit?(poll.options)
+    poll.errors.add(:options, duplicate_message) unless poll.options.uniq.size == poll.options.size
+  end
+
+  private
+
+  def too_few_message
+    I18n.t('polls.errors.too_few_options')
+  end
+
+  def too_many_message
+    I18n.t('polls.errors.too_many_options', max: MAX_OPTIONS)
+  end
+
+  def character_limit_message
+    I18n.t('polls.errors.over_character_limit', max: MAX_OPTION_CHARS)
+  end
+
+  def duplicate_message
+    I18n.t('polls.errors.duplicate_options')
+  end
+
+  def options_over_limit?(options)
+    options.any? { |option| option.each_grapheme_cluster.size > MAX_OPTION_CHARS }
   end
 end
