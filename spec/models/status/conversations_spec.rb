8c0f98360870da298d434929de7e697f459e4b56
Matt Jankowski <matt@jankowski.online>
2026-03-06 08:58:35 -0500

    Different approach to allow more isolated spec run

--- spec/models/status/conversations_spec.rb
+++ spec/models/status/conversations_spec.rb
@@ -2,7 +2,9 @@
 
 require 'rails_helper'
 
-RSpec.shared_examples 'Status::Conversations' do
+RSpec.describe Status::Conversations do
+  subject { Fabricate.build :status }
+
   describe 'Associations' do
     it { is_expected.to belong_to(:conversation).optional }
     it { is_expected.to have_one(:owned_conversation).class_name(Conversation).with_foreign_key(:parent_status_id).inverse_of(:parent_status).dependent(false) }
