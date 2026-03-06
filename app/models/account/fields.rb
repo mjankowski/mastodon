1640eaba30501e84052df9bfa430f7bebe1bc914
Matt Jankowski <matt@jankowski.online>
2026-03-06 17:38:46 -0500

    Fun and games

--- app/models/account/fields.rb
+++ app/models/account/fields.rb
@@ -13,43 +13,40 @@ module Account::Fields
   end
 
   def fields
-    (self[:fields] || []).filter_map do |f|
-      Account::Field.new(self, f)
+    Array(self[:fields]).filter_map do |field|
+      Account::Field.new(self, field)
     rescue
       nil
     end
   end
 
   def fields_attributes=(attributes)
-    fields     = []
-    old_fields = self[:fields] || []
-    old_fields = [] if old_fields.is_a?(Hash)
-
     attributes = attributes.values if attributes.is_a?(Hash)
 
-    attributes.each do |attr|
-      next if attr[:name].blank? && attr[:value].blank?
+    self[:fields] = [].tap do |fields|
+      attributes.each do |attr|
+        next if attr[:name].blank? && attr[:value].blank?
 
-      previous = old_fields.find { |item| item['value'] == attr[:value] }
+        normalized_previous_fields
+          .find { |field| field['value'] == attr[:value] }
+          .tap { |previous| attr[:verified_at] = previous && previous['verified_at'].presence }
 
-      attr[:verified_at] = previous['verified_at'] if previous && previous['verified_at'].present?
-
-      fields << attr
+        fields << attr
+      end
     end
-
-    self[:fields] = fields
   end
 
   def build_fields
     return if fields.size >= DEFAULT_FIELDS_SIZE
 
-    tmp = self[:fields] || []
-    tmp = [] if tmp.is_a?(Hash)
+    self.fields = normalized_previous_fields + additional_fields
+  end
 
-    (DEFAULT_FIELDS_SIZE - tmp.size).times do
-      tmp << { name: '', value: '' }
-    end
+  def additional_fields
+    Array.new(DEFAULT_FIELDS_SIZE - normalized_previous_fields.size) { %i(name value).index_with('') }
+  end
 
-    self.fields = tmp
+  def normalized_previous_fields
+    @normalized_previous_fields ||= Array(self[:fields].is_a?(Hash) ? nil : self[:fields])
   end
 end
