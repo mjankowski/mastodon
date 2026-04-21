7f93f8c20c9092cce77b00e5fcf26264fcf7207a
Matt Jankowski <matt@jankowski.online>
2026-04-21 12:10:46 -0400

    Prefer runnable route docs over inline comments

--- spec/requests/oauth/userinfo_spec.rb
+++ spec/requests/oauth/userinfo_spec.rb
@@ -11,41 +11,40 @@ RSpec.describe 'OAuth Userinfo Endpoint' do
   let(:scopes)   { 'profile' }
   let(:headers)  { { 'Authorization' => "Bearer #{token.token}" } }
 
-  shared_examples 'returns successfully' do
-    it 'returns http success' do
+  shared_examples 'oauth userinfo response' do
+    it 'returns http success and expected endpoint values' do
       subject
 
-      expect(response).to have_http_status(:success)
-      expect(response.content_type).to start_with('application/json')
-      expect(response.parsed_body).to include({
-        iss: root_url,
-        sub: ActivityPub::TagManager.instance.uri_for(account),
-        name: account.display_name,
-        preferred_username: account.username,
-        profile: short_account_url(account),
-        picture: full_asset_url(account.avatar_original_url),
-      })
+      expect(response)
+        .to have_http_status(:success)
+      expect(response.media_type)
+        .to eq('application/json')
+      expect(response.parsed_body)
+        .to include(
+          iss: root_url,
+          sub: ActivityPub::TagManager.instance.uri_for(account),
+          name: account.display_name,
+          preferred_username: account.username,
+          profile: short_account_url(account),
+          picture: full_asset_url(account.avatar_original_url)
+        )
     end
   end
+
+  # The spec requires both GET and POST for the userinfo endpoint:
+  # https://openid.net/specs/openid-connect-core-1_0.html#UserInfo
 
   describe 'GET /oauth/userinfo' do
-    subject do
-      get '/oauth/userinfo', headers: headers
-    end
+    subject { get '/oauth/userinfo', headers: headers }
 
     it_behaves_like 'forbidden for wrong scope', 'read:accounts'
-    it_behaves_like 'returns successfully'
+    it_behaves_like 'oauth userinfo response'
   end
 
-  # As this is borrowed from OpenID, the specification says we must also support
-  # POST for the userinfo endpoint:
-  # https://openid.net/specs/openid-connect-core-1_0.html#UserInfo
   describe 'POST /oauth/userinfo' do
-    subject do
-      post '/oauth/userinfo', headers: headers
-    end
+    subject { post '/oauth/userinfo', headers: headers }
 
     it_behaves_like 'forbidden for wrong scope', 'read:accounts'
-    it_behaves_like 'returns successfully'
+    it_behaves_like 'oauth userinfo response'
   end
 end
